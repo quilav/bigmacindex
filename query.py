78a1c64c2c3238a7be89a9b95f5d66f4c59f17e3
quilav <kjagodka11@gmail.com>
2022-02-12 20:19:46 +0100

    and another fix ;))

--- query.py
+++ query.py
@@ -5,4 +5,6 @@ mdf = pd.read_csv('big-mac-index.csv')
 
 semi_final = mdf.sort_values(['dollar_ppp', 'code'], ascending=False).groupby('code').head(1)
 final = semi_final.nlargest(5, 'dollar_ppp')
+
 print(final)
+final.to_csv('top5-bmi.csv')
