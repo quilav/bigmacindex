a5bb772944aa5abe9fa281e823b67839c6f10a83
quilav <kjagodka11@gmail.com>
2022-02-12 20:17:01 +0100

    added query needed for top 5 dollar_ppp values

---
+++ query.py
@@ -0,0 +1,8 @@
+import pandas as pd
+from pandas import DataFrame
+
+mdf = pd.read_csv('big-mac-index.csv')
+
+semi_final = mdf.sort_values(['dollar_ppp', 'code'], ascending=False).groupby('code').head(1)
+final = semi_final.nlargest(5, 'dollar_ppp')
+print(final)
